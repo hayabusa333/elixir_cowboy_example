d224d6d09683b48b56a976bc80d06e023cdff520
hayabusa333 <junnichi0121@gmail.com>
2017-01-08 09:31:37 +0900

    delete debug io

--- lib/elixir_cowboy_example/json_handler.ex
+++ lib/elixir_cowboy_example/json_handler.ex
@@ -6,7 +6,6 @@ defmodule ElixirCowboyExample.JsonHandler do
   def handle(req, state) do
     {method, req} = :cowboy_req.method(req)
     {:ok, params, req} = :cowboy_req.body_qs(req)
-    IO.inspect params
     {:ok, req} = json_example(method, params, req)
     {:ok, req, state}
   end
@@ -20,9 +19,7 @@ defmodule ElixirCowboyExample.JsonHandler do
   def json_example("POST", params, req) do
     param = params |> Enum.at(0) |> elem(1)
     headers = [{"content-type", "application/json"}]
-    IO.puts "3"
     body = "{\"title\": \"#{param}\"}"
-    IO.puts "4"
     {:ok, resp} = :cowboy_req.reply(200, headers, body, req)
   end
 
