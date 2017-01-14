3922e0c82e0a7dbd3a496292fc378852c0722bc5
hayabusa333 <junnichi0121@gmail.com>
2017-01-14 12:11:02 +0900

    read cookie

--- lib/elixir_cowboy_example/cookie_handler.ex
+++ lib/elixir_cowboy_example/cookie_handler.ex
@@ -1,5 +1,14 @@
 defmodule ElixirCowboyExample.CookieHandler do
   def init(req, opts) do
+
+    all_cookies = :cowboy_req.parse_cookies(req)
+    cookie_val = List.keyfind(all_cookies, :hoge, 0)
+
+    IO.inspect all_cookies
+    IO.inspect cookie_val
+
+    # IO.inspect :cowboy_req.match_cookies([:hoge], req)
+
     cookie_req = :cowboy_req.set_resp_cookie("server", "add_cookie", 
        %{max_age: 3600, domain: "localhost", path: "/"},
        req)
