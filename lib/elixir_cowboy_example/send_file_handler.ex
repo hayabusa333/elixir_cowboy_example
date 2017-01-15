92b01dd4d66b66d0a808077771ea0dd2f4ec2796
hayabusa333 <junnichi0121@gmail.com>
2017-01-15 14:37:32 +0900

    add send files

---
+++ lib/elixir_cowboy_example/send_file_handler.ex
@@ -0,0 +1,16 @@
+defmodule ElixirCowboyExample.SendFileHandler do
+  def init(req, opts) do
+
+    path = "priv/static/image/button.png"
+
+    %File.Stat{size: file_size} = File.stat!(path)
+
+    header = %{"content-type" => "image/png"}
+    body = "push cookie"
+
+    req = :cowboy_req.reply(200, header , 
+            {:sendfile, 0, file_size, path}, req)
+
+    {:ok, req, opts}
+  end
+end
